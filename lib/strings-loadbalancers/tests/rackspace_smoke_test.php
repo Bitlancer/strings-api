d0a439fd3cdcb0b683dcf399c205be37ac9ebeeb
Jesse Cotton <jcotton@bitlancer.com>
2013-12-28 18:45:18 +0000

    Rackspace LB smoke test

--- lib/strings-loadbalancers/tests/rackspace_smoke_test.php
+++ lib/strings-loadbalancers/tests/rackspace_smoke_test.php
@@ -2,133 +2,156 @@
 
 require('../Autoload.php');
 
-$connDetails = array(
-    'region' => 'DFW',
-    'identityApiEndpoint' => 'https://identity.api.rackspacecloud.com/v2.0/',
-    'credentials' => array(
-        'username' => 'bitlancer3',
-        'secret' => '3d0baf0e7b9c4882aefadaf0837ce61b'
-    )
-);
+$tester = new TestLoadBalancer();
+$tester->runTests();
 
-$conn = new \StringsLoadBalancer\RackspaceLoadBalancerDriver($connDetails);
+class TestLoadBalancer {
 
+    public function __construct(){
 
-//Create the LB
-$result = $conn->create(array(
-    'name' => 'Test',
-    'algorithm' => 'RANDOM',
-    'protocol' => 'HTTP',
-    'port' => 80,
-    'virtualIps' => array(
-        array(
-            'type' => 'PUBLIC'
-        )
-    )
-));
+        $connDetails = array(
+            'region' => 'DFW',
+            'identityApiEndpoint' => 'https://identity.api.rackspacecloud.com/v2.0/',
+            'credentials' => array(
+                'username' => 'bitlancer3',
+                'secret' => '3d0baf0e7b9c4882aefadaf0837ce61b'
+            )
+        );
 
-$lbId = $result['id'];
-
-$status = waitForActive($conn, $lbId);
-if($status != 'ACTIVE')
-    throw new \UnexpectedValueException("Unexpected load-balancer status $status");
-
-$result = $conn->get($lbId);
-$actualAttrs = array(
-    'name' => $result['name'],
-    'algorithm' => $result['algorithm'],
-    'protocol' => $result['protocol'],
-    'port' => $result['port']
-);
-$expectedAttrs = array(
-    'name' => 'Test',
-    'algorithm' => 'RANDOM',
-    'protocol' => 'HTTP',
-    'port' => '80'
-);
-if(!empty(array_diff($actualAttrs, $expectedAttrs))){
-    throw new \Exception('Load-balancer attributes do not match.');
-}
-
-
-//Update the LB
-$newAttrs = array(
-    'name' => 'Test-updated',
-    'algorithm' => 'LEAST_CONNECTIONS',
-    'protocol' => 'HTTPS',
-    'port' => '443'
-);
-$result = $conn->update($lbId, $newAttrs);
-
-$status = waitForActive($conn, $lbId);
-if($status != 'ACTIVE')
-    throw new \UnexpectedValueException("Unexpected load-balancer status $status");
-
-$result = $conn->get($lbId);
-$actualAttrs = array(
-    'name' => $results['name'],
-    'algorithm' => $results['algorithm'],
-    'protocol' => $results['protocol'],
-    'port' => $results['port']
-);
-$expectedAttrs = $newAttrs;
-if(!empty($array_diff($actualAttrs, $expectedAttrs))){
-    throw new \Exception('Load-balancer Attributes do not match.');
-}
-
-
-//Add nodes
-$result = $conn->addNodes($lbId, array(
-    array(
-        'address' => '10.7.7.1',
-        'port' => 80,
-        'condition' => 'ENABLED'
-    ),
-    array(
-        'address' => '10.7.7.2',
-        'port' => 80,
-        'condition' => 'ENABLED'
-    ),
-    array(
-        'address' => '10.7.7.3',
-        'port' => 80,
-        'condition' => 'ENABLED'
-    )
-));
-
-$status = waitForActive($conn, $lbId);
-if(!$status != 'ACTIVE')
-    throw new \UnexpectedValueException("Unexpected load-balancer status $status");
-
-
-//Get nodes
-$result = $conn->getNodes($lbId);
-print_r($result);
-
-
-//Remove Nodes
-$nodes = $conn->getNodes($lbId);
-
-
-//Delete the LB
-$conn->delete($lbId);
-
-
-function waitForActive($conn, $loadBalancerId, $pollInterval=10){
-
-    $status = false;
-
-    while(true){
-        $result = $conn->get($loadBalancerId);
-        $status = $result['loadBalancer']['status'];
-
-        if($status == 'ACTIVE')
-            break;
-        if($status == 'ERROR')
-            break;
-        else
-            sleep($pollInterval); 
+        $this->connection = new \StringsLoadBalancer\RackspaceLoadBalancerDriver($connDetails);
     }
 
-    return $status;
+    public function runTests(){
+
+        $lbId = $this->testCreate();
+        $this->testUpdate($lbId);
+        $this->testNodes($lbId);
+        //$this->testDelete($lbId);
+    }
+
+    private function testCreate(){
+
+        //Create the LB
+        $result = $this->connection->create(array(
+            'name' => 'Test',
+            'algorithm' => 'RANDOM',
+            'protocol' => 'HTTP',
+            'port' => 80,
+            'virtualIps' => array(
+                array(
+                    'type' => 'PUBLIC'
+                )
+            )
+        ));
+
+        $lbId = $result['id'];
+
+        $status = $this->waitForActive($lbId);
+        if($status != 'ACTIVE')
+            throw new \UnexpectedValueException("Unexpected load-balancer status $status");
+
+        $result = $this->connection->get($lbId);
+        $actualAttrs = array(
+            'name' => $result['name'],
+            'algorithm' => $result['algorithm'],
+            'protocol' => $result['protocol'],
+            'port' => $result['port']
+        );
+        $expectedAttrs = array(
+            'name' => 'Test',
+            'algorithm' => 'RANDOM',
+            'protocol' => 'HTTP',
+            'port' => '80'
+        );
+        $diff = array_diff($actualAttrs, $expectedAttrs);
+        if(!empty($diff)){
+            throw new \Exception('Load-balancer attributes do not match.');
+        }
+
+        return $lbId;
+    }
+
+    private function testUpdate($lbId){
+
+        //Update the LB
+        $newAttrs = array(
+            'name' => 'Test-updated',
+            'algorithm' => 'LEAST_CONNECTIONS',
+            'protocol' => 'HTTPS',
+            'port' => '443'
+        );
+        $result = $this->connection->update($lbId, $newAttrs);
+
+        $status = $this->waitForActive($lbId);
+        if($status != 'ACTIVE')
+            throw new \UnexpectedValueException("Unexpected load-balancer status $status");
+
+        $result = $this->connection->get($lbId);
+        $actualAttrs = array(
+            'name' => $results['name'],
+            'algorithm' => $results['algorithm'],
+            'protocol' => $results['protocol'],
+            'port' => $results['port']
+        );
+        $expectedAttrs = $newAttrs;
+        $diff = array_diff($actualAttrs, $expectedAttrs);
+        if(!empty($diff)){
+            throw new \Exception('Load-balancer Attributes do not match.');
+        }
+    }
+
+    private function testNodes($lbId){
+
+        //Add nodes
+        $result = $this->connection->addNodes($lbId, array(
+            array(
+                'address' => '10.7.7.1',
+                'port' => 80,
+                'condition' => 'ENABLED'
+            ),
+            array(
+                'address' => '10.7.7.2',
+                'port' => 80,
+                'condition' => 'ENABLED'
+            ),
+            array(
+                'address' => '10.7.7.3',
+                'port' => 80,
+                'condition' => 'ENABLED'
+            )
+        ));
+
+        $status = $this->waitForActive($lbId);
+        if(!$status != 'ACTIVE')
+            throw new \UnexpectedValueException("Unexpected load-balancer status $status");
+
+        //Get nodes
+        $result = $this->connection->getNodes($lbId);
+        print_r($result);
+
+    }
+
+    private function testDelete($lbId){
+
+        $this->connection->delete($lbId);
+    }
+
+    private function waitForActive($loadBalancerId, $pollInterval=10){
+
+        $status = false;
+
+        while(true){
+            $result = $this->connection->get($loadBalancerId);
+            $status = $result['status'];
+
+            if($status == 'ACTIVE')
+                break;
+            if($status == 'ERROR')
+                break;
+            else
+                sleep($pollInterval); 
+        }
+
+        return $status;
+    }
 }
